c71bdc325a3bb5a48ec5f32b26ad3d6e281638c4
norwellschmalz904 <norwellschmalz904@gmail.com>
2025-03-09 11:44:15 +0000

    Add 4.java

---
+++ src/4.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int x = 5;
+        while (x <= 10) {
+            System.out.println("Hello World");
+            x++;
+        }
+    }
+}

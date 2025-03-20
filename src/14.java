fc23ff0033d716fa45604cb34940f945a159314a
norwellschmalz904 <norwellschmalz904@gmail.com>
2025-03-20 05:30:28 +0000

    Add 14.java

---
+++ src/14.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+public class mySchoolProject{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello world!");
+        int a = input.nextInt();
+        System.out.println("The value of a is: " + a);
+        }
+}

9ee49f0aaca25dddd7da3a5b6804884bef121ce7
norwellschmalz904 <norwellschmalz904@gmail.com>
2025-04-01 17:53:01 +0000

    Add 22.java

---
+++ src/22.java
@@ -0,0 +1,6 @@
+public class SimpleExample {
+    public static void main(String[] args) {
+        // Example Java program to demonstrate basic coding concepts.
+        System.out.println("Hello, World!");
+    }
+}
